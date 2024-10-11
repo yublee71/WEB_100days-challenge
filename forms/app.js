b872afc22e7dbd66fd402b883f411407898087ed
yublee71 <lybin71@naver.com>
2024-10-11 11:52:03 +0100

    feat(forms): update js to add warning color for charcount

--- forms/app.js
+++ forms/app.js
@@ -1,8 +1,14 @@
 let textAreaElement = document.querySelector("textarea");
 let charCountElement = document.getElementById("charcount");
+let charDisplayElement = document.getElementById("char-display");
 
 function countNumofChar(event) {
   charCountElement.innerText = event.target.textLength;
+  if (event.target.textLength > 180) {
+    charCountElement.parentElement.classList.add("warning");
+  } else {
+    charCountElement.parentElement.classList.remove("warning");
+  }
 }
 
 textAreaElement.addEventListener("input", countNumofChar);
