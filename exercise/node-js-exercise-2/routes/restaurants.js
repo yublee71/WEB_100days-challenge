85d0184710b66b0f04b89ea6a0a6959d593ffb9d
yublee71 <lybin71@naver.com>
2024-10-18 18:47:46 +0100

    feat(node-js-exercise-2): add button to sort using queries

--- exercise/node-js-exercise-2/routes/restaurants.js
+++ exercise/node-js-exercise-2/routes/restaurants.js
@@ -24,9 +24,24 @@ router.post("/recommend", function (req, res) {
 
 router.get("/restaurants", function (req, res) {
   const storedRestaurants = resData.getStoredRestaurants();
+  let order = req.query.order;
+  let nextOrder = "desc";
+
+  if (order !== "asc" && order !== "desc") order = "asc";
+  if (order === "desc") nextOrder = "asc";
+
+  storedRestaurants.sort(function (resA, resB) {
+    if (
+      (order === "asc" && resA.name > resB.name) ||
+      (order === "desc" && resB.name > resA.name)
+    )
+      return 1;
+    return -1;
+  });
   res.render("restaurants", {
     numOfRes: storedRestaurants.length,
     restaurants: storedRestaurants,
+    nextOrder: nextOrder,
   });
 });
 
