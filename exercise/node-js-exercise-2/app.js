4e083ba48693a3ac3f72721f72e8fccd6f8c1cf5
yublee71 <lybin71@naver.com>
2024-10-18 15:21:24 +0100

    feat(node-js-exercise-2): add dedicated page for each restaurant and handle 404/500 errors

--- exercise/node-js-exercise-2/app.js
+++ exercise/node-js-exercise-2/app.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
+const uuid = require("uuid");
 const app = express();
 
 app.set("views", path.join(__dirname, "views")); //where to find template files
@@ -9,6 +10,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
+/*----without ejs----*/
 // app.get("/", function (req, res) {
 //   const filePath = path.join(__dirname, "views", "index.html");
 //   res.sendFile(filePath);
@@ -26,6 +28,7 @@ app.use(express.urlencoded({ extended: false }));
 //   res.sendFile(filePath);
 // });
 
+/*----with ejs----*/
 app.get("/", function (req, res) {
   res.render("index"); //no need to put file extention or path thanks to line6&7
 });
@@ -44,6 +47,7 @@ app.get("/recommend", function (req, res) {
 
 app.post("/recommend", function (req, res) {
   const restaurant = req.body;
+  restaurant.id = uuid.v4();
   const filePath = path.join(__dirname, "data", "restaurants.json");
   const fileData = fs.readFileSync(filePath);
   const storedRestaurants = JSON.parse(fileData);
@@ -61,6 +65,27 @@ app.get("/restaurants", function (req, res) {
     numOfRes: storedRestaurants.length,
     restaurants: storedRestaurants,
   });
+});
+
+app.get("/restaurants/:restaurantid", function (req, res) {
+  const restaurantId = req.params.restaurantid;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+  for (const restaurant of storedRestaurants) {
+    if (restaurant.id === restaurantId) {
+      return res.render("restaurant-detail", { restaurant: restaurant });
+    }
+  }
+  res.status(404).render("404"); // could return as well but not necessary
+});
+
+app.use(function (req, res) {
+  res.status(404).render("404");
+});
+
+app.use(function (error, req, res, next) {
+  res.status(500).render("500");
 });
 
 app.listen(3000);
